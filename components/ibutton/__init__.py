54b05ce6f671b0ebd6f889d350a36d944010901e
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-07-15 23:07:18 +0300

    Update __init__.py

--- components/ibutton/__init__.py
+++ components/ibutton/__init__.py
@@ -1,9 +1,6 @@
-CODEOWNERS = ["@sergeyka2021"]
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
-from esphome.const import CONF_ID, CONF_PIN
+from esphome.const import CONF_ID, CONF_PIN, CONF_KEY_ID
 
 DEPENDENCIES = []
 AUTO_LOAD = ['sensor']
@@ -14,12 +11,12 @@ IButton = ibutton_ns.class_('IButton', cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(IButton),
     cv.Required(CONF_PIN): cv.int_,
-    cv.Optional('key_id'): cv.hex_uint64_t,
+    cv.Optional(CONF_KEY_ID): cv.hex_uint64_t,
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     cg.add(var.set_pin(config[CONF_PIN]))
-    if 'key_id' in config:
-        cg.add(var.set_key_id(config['key_id']))
+    if CONF_KEY_ID in config:
+        cg.add(var.set_key_id(config[CONF_KEY_ID]))
