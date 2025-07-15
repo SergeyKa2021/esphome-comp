cd5f57b98d7cf084a2ab7e1b9e7339dfaa14411d
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-07-15 23:21:52 +0300

    Update __init__.py

--- components/ibutton/__init__.py
+++ components/ibutton/__init__.py
@@ -3,25 +3,23 @@ import esphome.config_validation as cv
 from esphome.components import button
 from esphome.const import CONF_ID, CONF_PIN, CONF_KEY_ID
 
-CODEOWNERS = ["@yourusername"]
+CODEOWNERS = ["@SergeyKa2021"]
 DEPENDENCIES = []
 AUTO_LOAD = ["sensor"]
 
 ibutton_ns = cg.esphome_ns.namespace("ibutton")
 IButtonComponent = ibutton_ns.class_("IButtonComponent", cg.Component)
 
-CONFIG_SCHEMA = cv.All(
-    cv.Schema({
-        cv.GenerateID(): cv.declare_id(IButtonComponent),
-        cv.Required(CONF_PIN): cv.int_,
-        cv.Optional(CONF_KEY_ID): cv.uint64_t,
-        cv.Optional("write_button"): cv.use_id(button.Button),
-    }).extend(cv.COMPONENT_SCHEMA)
-)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(IButtonComponent),
+    cv.Required(CONF_PIN): cv.int_,
+    cv.Optional(CONF_KEY_ID): cv.uint64_t,
+    cv.Optional("write_button"): cv.use_id(button.Button),
+}).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+    await cg.register_component(var, config)
     
     cg.add(var.set_pin(config[CONF_PIN]))
     
@@ -29,5 +27,5 @@ def to_code(config):
         cg.add(var.set_key_id(config[CONF_KEY_ID]))
     
     if "write_button" in config:
-        button_var = yield cg.get_variable(config["write_button"])
+        button_var = await cg.get_variable(config["write_button"])
         cg.add(var.set_write_button(button_var))
