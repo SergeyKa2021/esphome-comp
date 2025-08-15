344102b51edddded10d90ff7a00a89f008001b48
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-15 23:25:00 +0300

    Update __init__.py

--- components/dallas_key_reader/__init__.py
+++ components/dallas_key_reader/__init__.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
 from esphome.const import CONF_ID
-from esphome.components.one_wire import OneWireComponent
+from esphome.components.one_wire import OneWireDevice
 
 CODEOWNERS = ["@SergeyKa2021"]
 DEPENDENCIES = ["one_wire", "text_sensor"]
@@ -12,7 +12,7 @@ DallasKeyReader = dallas_key_reader_ns.class_("DallasKeyReader", cg.PollingCompo
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(DallasKeyReader),
-    cv.Required("one_wire_id"): cv.use_id(OneWireComponent),
+    cv.Required("one_wire_id"): cv.use_id(OneWireDevice),
     cv.Required("key"): text_sensor.text_sensor_schema(),
 }).extend(cv.polling_component_schema("60s"))
 
