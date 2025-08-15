2d138d5aa7d7aca9c25b5401cd390a05bd21475f
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-15 23:09:46 +0300

    Update __init__.py

--- components/dallas_key_reader/__init__.py
+++ components/dallas_key_reader/__init__.py
@@ -2,19 +2,17 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
 from esphome.const import CONF_ID
-from esphome.components.one_wire.one_wire import OneWire
+from esphome.components.one_wire.one_wire import OneWireComponent
 
 CODEOWNERS = ["@SergeyKa2021"]
 DEPENDENCIES = ["one_wire", "text_sensor"]
 
-dallas_key_reader_ns = cg.esphome_ns.namespace("dallas_key_reader")  # Исправлено!
-DallasKeyReader = dallas_key_reader_ns.class_(
-    "DallasKeyReader", cg.PollingComponent
-)
+dallas_key_reader_ns = cg.esphome_ns.namespace("dallas_key_reader")
+DallasKeyReader = dallas_key_reader_ns.class_("DallasKeyReader", cg.PollingComponent)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(DallasKeyReader),
-    cv.Required("one_wire"): cv.use_id(OneWire),
+    cv.Required("one_wire_id"): cv.use_id(OneWireComponent),
     cv.Required("key"): text_sensor.text_sensor_schema(),
 }).extend(cv.polling_component_schema("60s"))
 
@@ -22,7 +20,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    one_wire = await cg.get_variable(config["one_wire"])
+    one_wire = await cg.get_variable(config["one_wire_id"])
     cg.add(var.set_one_wire(one_wire))
     
     key_sensor = await text_sensor.new_text_sensor(config["key"])
