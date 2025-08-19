0451c15aa841d60a33527fddd2c95347c5584ec5
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-20 00:29:20 +0300

    Update text_sensor.py

--- components/ds1990_key/text_sensor.py
+++ components/ds1990_key/text_sensor.py
@@ -10,7 +10,7 @@ ds1990_key_ns = cg.esphome_ns.namespace('ds1990_key')
 DS1990KeySensor = ds1990_key_ns.class_('DS1990KeySensor', text_sensor.TextSensor, cg.Component)
 
 CONFIG_SCHEMA = text_sensor.text_sensor_schema().extend({
-    cv.GenerateID(): cv.declare_id(DS1990KeyTextSensor),
+    cv.GenerateID(): cv.declare_id(DS1990KeySensor),
     cv.Required(CONF_ADDRESS): cv.hex_uint64_t,
     cv.Optional(one_wire.CONF_ONE_WIRE_ID): cv.use_id(one_wire.OneWireBus),
 })
