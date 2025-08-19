edf3c65aa03562d71a285ca8953b855184928b2f
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-20 00:27:17 +0300

    Update ds1990_key.h

--- components/ds1990_key/ds1990_key.h
+++ components/ds1990_key/ds1990_key.h
@@ -12,22 +12,33 @@ static const uint8_t DS1990A_FAMILY_CODE = 0x01;
 static const uint8_t DS1990R_FAMILY_CODE = 0x81;
 static const uint8_t READ_ROM = 0x33;
 
+// Наследник OneWireBus с публичными методами
+class DS1990OneWire : public one_wire::OneWireBus {
+ public:
+  using one_wire::OneWireBus::OneWireBus;
+  
+  bool public_reset() { return this->reset_(); }
+  void public_reset_search() { this->reset_search(); }
+  bool public_search(uint8_t *rom_code) { return this->search(rom_code); }
+  void public_write8(uint8_t value) { this->write8(value); }
+  uint8_t public_read8() { return this->read8(); }
+  void public_skip() { this->write8(0xCC); } // SKIP_ROM
+};
+
 class DS1990KeySensor : public text_sensor::TextSensor, public Component {
  public:
-  void set_one_wire(one_wire::OneWireBus *one_wire) { one_wire_ = one_wire; }
+  void set_one_wire(DS1990OneWire *one_wire) { one_wire_ = one_wire; }
   
   void setup() override;
   void dump_config() override;
-  
-  // Убрали update() так как теперь это не PollingComponent
-  void loop() override;  // Будем читать в loop()
+  void loop() override;
   
   float get_setup_priority() const override { return setup_priority::DATA; }
 
  protected:
   bool read_key_data_();
   
-  one_wire::OneWireBus *one_wire_;
+  DS1990OneWire *one_wire_;
   uint64_t address_{0};
   uint32_t last_read_{0};
 };
