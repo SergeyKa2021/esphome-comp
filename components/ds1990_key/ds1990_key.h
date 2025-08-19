2e29ae0c44dcbb85647068a94671824db151e9fe
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-19 22:55:18 +0300

    Update ds1990_key.h

--- components/ds1990_key/ds1990_key.h
+++ components/ds1990_key/ds1990_key.h
@@ -1,29 +1,32 @@
 #pragma once
 
-#include "esphome/core/component.h"
+#include "esphome.h"
 #include "esphome/components/one_wire/one_wire.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 
 namespace esphome {
 namespace ds1990_key {
 
-class DS1990KeySensor : public Component, public text_sensor::TextSensor {
+// Коды семейств DS1990
+static const uint8_t DS1990A_FAMILY_CODE = 0x01;
+static const uint8_t DS1990R_FAMILY_CODE = 0x81;
+static const uint8_t READ_ROM = 0x33;
+
+class DS1990KeySensor : public text_sensor::TextSensor, public PollingComponent {
  public:
-  void set_address(uint64_t address);
-  void set_one_wire(one_wire::OneWireBus *one_wire);
+  void set_one_wire(one_wire::OneWireBus *one_wire) { one_wire_ = one_wire; }
   
   void setup() override;
   void dump_config() override;
-  float get_setup_priority() const override;
-  
   void update() override;
+  
+  float get_setup_priority() const override { return setup_priority::DATA; }
 
  protected:
+  bool read_key_data_();
+  
   one_wire::OneWireBus *one_wire_;
   uint64_t address_{0};
-  bool found_{false};
-  
-  bool read_key_data_();
 };
 
 }  // namespace ds1990_key
