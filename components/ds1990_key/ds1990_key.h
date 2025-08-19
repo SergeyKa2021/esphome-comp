c937c349db05d668c9484e23094c3574a4c191ed
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-19 22:06:22 +0300

    Create ds1990_key.h

---
+++ components/ds1990_key/ds1990_key.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/one_wire/one_wire.h"
+
+namespace esphome {
+namespace ds1990_key {
+
+class DS1990KeySensor : public Component, public sensor::Sensor {
+ public:
+  void set_address(uint64_t address);
+  void set_one_wire(one_wire::OneWire *one_wire);
+  
+  void setup() override;
+  void dump_config() override;
+  float get_setup_priority() const override;
+  
+  void update() override;
+
+ protected:
+  one_wire::OneWire *one_wire_;
+  uint64_t address_{0};
+  bool found_{false};
+  
+  bool read_key_data_();
+};
+
+}  // namespace ds1990_key
+}  // namespace esphome
