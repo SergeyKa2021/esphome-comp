b33a789e40231e92c41db3f32c65fbf0d830cf98
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-19 22:15:10 +0300

    Update __init__.py

--- components/ds1990_key/__init__.py
+++ components/ds1990_key/__init__.py
@@ -1,21 +1,8 @@
-CODEOWNERS = ["@Sergeyka2021"]
-
-from esphome.components.sensor import Sensor
+import esphome.codegen as cg
+import esphome.config_validation as cv
 from esphome import automation
 from esphome.automation import maybe_simple_id
 
-DS1990KeySensor = Sensor.class_('DS1990KeySensor', Sensor)
+CODEOWNERS = ["@Sergeyka2021"]
 
-# Automation to trigger key reading
-@automation.register_action(
-    "ds1990_key.read",
-    maybe_simple_id(
-        {
-            cv.GenerateID(): cv.use_id(DS1990KeySensor),
-        }
-    ),
-)
-async def ds1990_key_read_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
+# Empty __init__ since we're using sensor platform
