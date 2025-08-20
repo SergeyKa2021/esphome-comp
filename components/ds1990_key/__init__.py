0947f552c349298ad458ee0bc3fafebec4a8b2d7
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-20 09:39:04 +0300

    Update __init__.py

--- components/ds1990_key/__init__.py
+++ components/ds1990_key/__init__.py
@@ -1,8 +1,3 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome import automation
-from esphome.automation import maybe_simple_id
-
 CODEOWNERS = ["@Sergeyka2021"]
 
 # Empty __init__ since we're using sensor platform
