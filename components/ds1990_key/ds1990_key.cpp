63998fd4048dd87088903c78242ce84213fc7f22
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-20 20:32:33 +0300

    Update ds1990_key.cpp

--- components/ds1990_key/ds1990_key.cpp
+++ components/ds1990_key/ds1990_key.cpp
@@ -33,7 +33,10 @@ void DS1990KeySensor::update() {
 
 bool DS1990KeySensor::read_key_data_() {
   uint8_t rom_code[8];
-  
+
+  if (!this->bus_->reset_()) {
+    return;
+  }
   // Пропускаем выбор устройства
   // this->skip();
   // Отправляем команду чтения ROM
