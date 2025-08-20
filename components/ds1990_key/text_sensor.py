aa35a7546c46dedf9e16712a92bcd91f5148422c
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-20 11:27:16 +0300

    Update text_sensor.py

--- components/ds1990_key/text_sensor.py
+++ components/ds1990_key/text_sensor.py
@@ -13,6 +13,8 @@ CONFIG_SCHEMA = text_sensor.text_sensor_schema().extend({
     cv.GenerateID(): cv.declare_id(DS1990KeySensor),
     cv.Required(CONF_ADDRESS): cv.hex_uint64_t,
 })
+    .extend(one_wire.one_wire_device_schema())
+    .extend(cv.polling_component_schema("5s"))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
