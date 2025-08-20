b1cd43c13e8d52fddb636a47a1a6c870cab6dd43
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-20 09:56:06 +0300

    Update text_sensor.py

--- components/ds1990_key/text_sensor.py
+++ components/ds1990_key/text_sensor.py
@@ -7,21 +7,20 @@ DEPENDENCIES = ['one_wire']
 AUTO_LOAD = ['text_sensor']
 
 ds1990_key_ns = cg.esphome_ns.namespace('ds1990_key')
-DS1990KeySensor = ds1990_key_ns.class_('DS1990KeySensor', text_sensor.TextSensor, cg.Component)
+DS1990KeySensor = ds1990_key_ns.class_('DS1990KeySensor', text_sensor.TextSensor, cg.PollingComponent)
 
 CONFIG_SCHEMA = text_sensor.text_sensor_schema().extend({
     cv.GenerateID(): cv.declare_id(DS1990KeySensor),
     cv.Required(CONF_ADDRESS): cv.hex_uint64_t,
-    cv.Optional(one_wire.CONF_ONE_WIRE_ID): cv.use_id(one_wire.OneWireBus),
+    cv.Optional(one_wire.CONF_ONE_WIRE_ID): cv.use_id(one_wire.OneWireDevice),
+    .extend(cv.polling_component_schema("5s"))
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await text_sensor.register_text_sensor(var, config)
+    await one_wire.register_one_wire_device(var, config)
     
     cg.add(var.set_address(config[CONF_ADDRESS]))
     
-    if one_wire.CONF_ONE_WIRE_ID in config:
-        one_wire_bus = await cg.get_variable(config[one_wire.CONF_ONE_WIRE_ID])
-        cg.add(var.set_one_wire(one_wire_bus))
