e2fd6caa188dc14135404a15dbf8d2ffdfa9b0d5
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-08-20 23:10:20 +0300

    Update ds1990_key.h

--- components/ds1990_key/ds1990_key.h
+++ components/ds1990_key/ds1990_key.h
@@ -12,19 +12,19 @@ static const uint8_t DS1990A_FAMILY_CODE = 0x01;
 static const uint8_t DS1990R_FAMILY_CODE = 0x81;
 static const uint8_t READ_ROM = 0x33;
 
-class DS1990KeySensor : public PollingComponent, public text_sensor::TextSensor, public one_wire::OneWireDevice {
+class DS1990KeySensor : public PollingComponent, public text_sensor::TextSensor {
  public:
   void setup() override;
   void dump_config() override;
   void update() override;
-  void set_bus(one_wire::OneWire *bus) { bus_ = bus; }
+  void set_bus(one_wire::OneWireBus *bus) { bus_ = bus; }
   
   float get_setup_priority() const override { return setup_priority::DATA; }
 //  one_wire::OneWireBus *bus_;
 
  protected:
   bool read_key_data_();
-  one_wire::OneWire *bus_;
+  one_wire::OneWireBus *bus_;
   uint64_t address_{0};
 };
 
